50120bafaceca2107a7f6aba677f3890b94901f3
Nguyen Anh Hao <contactnguyenanhhao@gmail.com>
2025-06-18 03:12:33 +0700

    level 2 wip

---
+++ 2DPlatformer/Assets/Scripts/General/Levels/MovingPlatform.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
